9f5c7f03743d14f3ce4d6925d48d6ed737fce771
Brian <bapowell00@gmail.com>
2018-05-10 00:27:09 -0700

    updated server.js to handle social logins

--- server/server.js
+++ server/server.js
@@ -9,18 +9,6 @@ var loopBackPassPort = require('loopback-component-passport');
 var PassportConfigurator = loopBackPassPort.PassportConfigurator;
 var passportConfigurator = PassportConfigurator(app);
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
@@ -32,6 +20,10 @@ boot(app, __dirname, function(err) {
     app.start();
 });
 
+//enable http session
+app.use(loopback.session({secret: 'looforpoops'}));
+
+//attempto build the providers/passport config
 var config = {};
 try {
   config = require('./providers.json');
