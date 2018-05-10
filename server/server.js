5be2772dd8a0742b80dd17c64bf3d85fbef32d70
Brian <bapowell00@gmail.com>
2018-05-10 01:38:45 -0700

    finnally fixed, i hope

--- server/server.js
+++ server/server.js
@@ -5,18 +5,10 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
+var loopBackPassPort = require('loopback-component-passport');
+var PassportConfigurator = loopBackPassPort.PassportConfigurator;
+var passportConfigurator = PassportConfigurator(app);
+
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
@@ -27,3 +19,32 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+//enable http session
+app.use(loopback.session({secret: 'looforpoops'}));
+
+//attempto build the providers/passport config
+var config = {};
+try {
+  config = require('./providers.json');
+} catch (err) {
+  console.trace(err);
+  process.exit(1); // fatal exit
+}
+
+//initialize passport
+passportConfigurator.init();
+
+//set up related models
+passportConfigurator.setupModels({
+  userModel: app.models.user,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential
+});
+
+//configure passport strategies for third party auth providers
+for(var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
