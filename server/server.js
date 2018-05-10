7b8e32d1732e73115fe6527575f87bc10ffae8b6
Brian <bapowell00@gmail.com>
2018-05-10 13:31:21 -0700

    reverting to remove oauth, doesn't work

--- server/server.js
+++ server/server.js
@@ -6,11 +6,6 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-var loopBackPassPort = require('loopback-component-passport');
-var PassportConfigurator = loopBackPassPort.PassportConfigurator;
-var passportConfigurator = PassportConfigurator(app);
-
-
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
@@ -21,35 +16,3 @@ boot(app, __dirname, function(err) {
     app.start();
 });
 
-//enable http session
-app.use(loopback.session({ 
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitalized: true
-}));
-
-//attempto build the providers/passport config
-var config = {};
-try {
-  config = require('./providers.json');
-} catch (err) {
-  console.trace(err);
-  process.exit(1); // fatal exit
-}
-
-//initialize passport
-passportConfigurator.init();
-
-//set up related models
-passportConfigurator.setupModels({
-  userModel: app.models.account,
-  userIdentityModel: app.models.userIdentity,
-  userCredentialModel: app.models.userCredential
-});
-
-//configure passport strategies for third party auth providers
-for(var s in config) {
-  var c = config[s];
-  c.session = c.session !== false;
-  passportConfigurator.configureProvider(s, c);
-}
